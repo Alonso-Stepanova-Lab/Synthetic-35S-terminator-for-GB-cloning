cfa9703bc560ba9be03b95cf7051a68800fd12e6
Alonso_Stepanova Lab <jmalonso@ncsu.edu>
2019-05-02 09:14:38 -0400

    Update MixterminatorControl.pl

--- MixterminatorControl.pl
+++ MixterminatorControl.pl
@@ -1,26 +1,22 @@
-open (OUT,">random35S.txt");
+open (OUT,">random35STerminatorControl.txt");
 use List::Util qw(shuffle);
 $contador=0;
 @data1 = ("a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","g","g","g","g","g","g","g","g","g","g",
-         "g","g","g","g","g","g","g","g","g","g","g","g","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t");
-@data2 = ("a","a","a","a","a","t","t","t","t","t","c","c","c");
-@data3 = ("a","a","a","a","a","a","a","a","a","a","a","a","t","t","t","t","t","t","t","t","c","c","c","c","c","c","c","g");
-@data4 = ("t","t","a","g","t","a","t","g","t","a","t","t","t","g","t","a","t","t","t","g","t","a");
-@data5 = ("a","a","t","a","a","a");
+         "g","g","g","g","g","g","g","g","g","g","g","g","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","t","a","a","a","a","a","t","t","t","t","t","c","c","c","a","a","a","a","a","a","a","a","a","a","a","a","t","t","t","t","t","t","t","t","c","c","c","c","c","c","c","g","t","t","a","g","t","a","t","g","t","a","t","t","t","g","t","a","t","t","t","g","t","a","a","a","t","a","a","a");
 
-for ($i=1;$i<5000000;$i++){
+for ($i=1;$i<500000000;$i++){
 @cards1 = shuffle @data1;
-@cards2 = shuffle @data2;
-@cards3 = shuffle @data3;
-@cards4 = shuffle @data4;
-@cards5 = shuffle @data5;
+#@cards2 = shuffle @data2;
+#@cards3 = shuffle @data3;
+#@cards4 = shuffle @data4;
+#@cards5 = shuffle @data5;
  $string1=join("",@cards1);
-  $string2=join("",@cards2);
-   $string3=join("",@cards3);
-   $string4=join("",@cards4);
-   $string5=join("",@cards5);
+#  $string2=join("",@cards2);
+#   $string3=join("",@cards3);
+#   $string4=join("",@cards4);
+#   $string5=join("",@cards5);
    
-   $string="$string1$string4$string2$string5$string3";
+   $string=$string1;
     $test=0;
     
     
@@ -38,6 +34,6 @@ for ($i=1;$i<5000000;$i++){
         if ($string=~m/cg/) {$test=1}
         if ($string=~m/c[a,c,g,t]g/) {$test=1}
 
-    if ($test==0){$contador++;$firstpart=substr($string,0,120); $secondpart=substr($string,121,13); $thirdpart=substr($string,133,28); print OUT">35STerm$contador\n"; print OUT"$string1"; print OUT"$string4"; print OUT"$string2"; print OUT"$string5"; print OUT"$string3\n"}
+    if ($test==0){$contador++;$firstpart=substr($string,0,120); $secondpart=substr($string,121,13); $thirdpart=substr($string,133,28); print OUT">35STerm$contador\n"; print OUT"$string1\n"}
 
 }
